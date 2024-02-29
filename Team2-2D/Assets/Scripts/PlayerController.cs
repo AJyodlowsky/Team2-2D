f7e8eca29d5ddd4fc89d14e7b90d76eb6233e8ba
AJyodlowsky <ajyodlowsky@my.waketech.edu>
2024-02-29 09:00:27 -0500

    Added Second Asset Pack, HELP menu
    read

--- Team2-2D/Assets/Scripts/PlayerController.cs
+++ Team2-2D/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Written by Jancy
+
 public class PlayerController : MonoBehaviour
 {
     private float horizontal;
