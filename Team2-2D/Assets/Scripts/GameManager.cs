85c00e0d750e0f2034d641a5e14394e93cb59a74
BasedPlant <146005173+BasedPlant@users.noreply.github.com>
2024-02-29 10:46:53 -0500

    Level one Coding Halfway

--- Team2-2D/Assets/Scripts/GameManager.cs
+++ Team2-2D/Assets/Scripts/GameManager.cs
@@ -5,19 +5,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject darkMode;
+    [SerializeField] private GameObject lightMode;
+    [SerializeField] private KeyCode activatedarkToggleKey = KeyCode.P;
+    [SerializeField] private KeyCode deactivatedarkToggleKey = KeyCode.O;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        if(gameObject.tag == "Dark")
-        {
-            gameObject.SetActive(false);
-        }
+        darkMode.SetActive(false);
+        lightMode.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(activatedarkToggleKey))
+        {
+
+            Debug.Log("Dark Mode Was Activated");
+            darkMode.SetActive(true);
+            lightMode.SetActive(false);
+
+        }
+        if (Input.GetKeyDown(deactivatedarkToggleKey))
+        {
+
+            Debug.Log("Dark Mode Was Activated");
+            darkMode.SetActive(false);
+            lightMode.SetActive(true);
+        }
+
     }
+
+    
 }
+   
+
--- Team2-2D/Assets/Scripts/PlayerController.cs
+++ Team2-2D/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.SceneManagement;
 //Written by Jancy
 
 public class PlayerController : MonoBehaviour
@@ -15,9 +16,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private int berriesCollected;
+    [SerializeField] private TMP_Text berryScore;
+
 
     void Update()
     {
+        berryScore.text = "Score: " + berriesCollected;
         horizontal = Input.GetAxisRaw("Horizontal");
         if (IsGrounded() && !Input.GetButton("Jump"))
         {
@@ -56,6 +61,24 @@ public class PlayerController : MonoBehaviour
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Berry")
+        {
+            other.gameObject.SetActive(false);
+            berriesCollected++;
+        }
+        if (other.gameObject.tag == "Hazard") 
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+        if (other.gameObject.tag == "Goal" && berriesCollected == 3)
+        {
+
+            SceneManager.LoadScene("LevelTwo");
         }
     }
 }
